20c977d1877407e9390c7deb02d368380bcc2f15
Your Name <christopherarie147@gmail.com>
2023-12-05 19:38:58 +0700

    revisi dan session login

--- backend/hapus/proses-hapus-berita.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('../../connection.php');
-
-$id = $_GET["idBerita"];
-
-$query = mysqli_query($conn, "DELETE FROM `berita` WHERE `id_berita` = $id;");
-
-header('Location:../../admin/berita.php');
--- backend/hapus/proses-hapus-saran.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('../../connection.php');
-
-$id = $_GET["idSaran"];
-
-$query = mysqli_query($conn, "DELETE FROM `saran_berita` WHERE `id_saran_berita` = $id;");
-
-header('Location:../../admin/saran.php');
