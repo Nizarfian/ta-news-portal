87013723860730ffa4b129b467f0cee828d79b07
Your Name <christopherarie147@gmail.com>
2023-12-14 20:15:52 +0700

    cek email

--- backend/tambah/proses-tambah-user.php
+++ backend/tambah/proses-tambah-user.php
@@ -6,6 +6,18 @@ $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = mysqli_query($conn, "INSERT INTO `user` (`id_user`, `email`, `username`, `password`, `role`) VALUES (NULL,'$email', '$username', '$password', 'user');");
+$query = mysqli_query($conn, "SELECT * FROM `user` WHERE email='$email'");
+$cekEmail = mysqli_fetch_assoc($query);
 
-header('Location:../../login/login.php');
+if ($cekEmail) {
+  echo "
+        <script>
+        alert('Email sudah digunakan. Gunakan email lain.');
+        window.location.href = '../../login/sign-up.php';
+        </script>
+    ";
+} else {
+  $query = mysqli_query($conn, "INSERT INTO `user` (`id_user`, `email`, `username`, `password`, `role`) VALUES (NULL, '$email', '$username', '$password', 'user');");
+
+  header('Location:../../login/login.php');
+}
